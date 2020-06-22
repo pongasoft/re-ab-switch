04bf745cc4eaba2f5591be53161a32f44edbdae5
Yan Pujante <yan@pongasoft.com>
2020-06-22 11:38:33 -0700

    Migrated to use re-cmake / RE SDK 4.2.0

--- GUI2D/hdgui_2D.lua
+++ GUI2D/hdgui_2D.lua
@@ -1,4 +1,4 @@
-format_version = "1.0"
+format_version = "2.0"
 Q=5
 
 front = jbox.panel{
--- jukebox.py
+++
@@ -1,4 +0,0 @@
-import os
-
-JUKEBOX_ROOT_DIR = "/Volumes/Vault/Applications/JukeboxSDK_2.2"
-JUKEBOX_SDK_DIR = os.path.join(JUKEBOX_ROOT_DIR, "SDK")
